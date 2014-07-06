6c2062d5a1169324f5c711e577305db5706c44f5
hirsch <hirsch@blinkenshell.org>
2014-07-06 19:36:56 +0200

    Included Example

--- eperf.go
+++ eperf.go
@@ -25,7 +25,13 @@ func New(name string, cycles int) *Perftest {
 
 // Run runs the given Perftest. Use within a for loop.
 // The fastest execution time of all cycles will be printed.
-p *Perftest) Run() bool {
+//	perftest := eperf.New("code test", 1000)	// Create a new Perftest
+//	for perftest.Run() {						// Use Run() in a loop
+//		code()									// code() will be executed 1000 times.
+//	}
+//	// The fastest execution time will be logged. Example output:
+//	// 2014/07/06 17:26:59 eperf: code test runtime: 133.806us
+func (p *Perftest) Run() bool {
 	if p.round > p.cycles {
 		if p.round > 1 {
 			log.Println("eperf:", p.name, "runtime:", p.min)
